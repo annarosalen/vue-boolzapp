e49a4f3f6abe58b44cd741f01ce9c0d3dfacb81d
Anna <rosalenanna@gmail.com>
2020-11-24 18:14:31 +0100

    funzione data e ora

--- js/myscript.js
+++ js/myscript.js
@@ -13,16 +13,19 @@ var app= new Vue({
         chat: [
           {
             msg: "ciao come stai?",
+            date: "10/11/2020 13:44",
             stato: "msg-sent",
             posizione: "box-msg-right"
           },
           {
             msg: "ciao tutto bene",
+            date: "10/11/2020 14:00",
             stato: "msg-received",
             posizione: "box-msg-left"
           },
           {
             msg:"domani cosa fai?",
+            date: "10/11/2020 14:00",
             stato: "msg-received",
             posizione: "box-msg-left"
           }
@@ -37,11 +40,13 @@ var app= new Vue({
         chat: [
           {
             msg: "ciaooooo?",
+            date: "30/11/2020 13:20",
             stato: "msg-sent",
             posizione: "box-msg-right"
           },
           {
             msg: "a domani",
+            date: "30/11/2020 15:00",
             stato: "msg-received",
             posizione: "box-msg-left"
           },
@@ -55,16 +60,19 @@ var app= new Vue({
         chat: [
           {
             msg: "Hola como estas?",
+            date: "29/10/2020 09:50",
             stato: "msg-sent",
             posizione: "box-msg-right"
           },
           {
             msg: "Holiii, mañana nos vamos de fiesta",
+            date: "29/10/2020 12:20",
             stato: "msg-received",
             posizione: "box-msg-left"
           },
           {
             msg:"te vas a juntar con nosotros?",
+            date: "29/10/2020 12:21",
             stato: "msg-received",
             posizione: "box-msg-left"
           }
@@ -79,11 +87,13 @@ var app= new Vue({
         chat: [
           {
             msg:"Hi I'll call you tomorrow",
+            date: "31/10/2020 14:20",
             stato: "msg-sent",
             posizione:"box-msg-right"
           },
           {
             msg:"ok,bye",
+            date: "31/10/2020 14:23",
             stato:"msg-received",
             posizione:"box-msg-left"
           }
@@ -97,7 +107,7 @@ var app= new Vue({
     },
 
     enterMsg: function(){
-      this.contatti[this.contattiIndex].chat.push({msg: this.message, stato: "msg-sent", posizione: "box-msg-right"});
+      this.contatti[this.contattiIndex].chat.push({msg: this.message, date: this.currentTime(), stato: "msg-sent", posizione: "box-msg-right"});
 
       this.message="";
 
@@ -105,9 +115,16 @@ var app= new Vue({
     },
 
     getMsg: function(){
-      this.contatti[this.contattiIndex].chat.push({msg: "ok", stato: "msg-received", posizione: "box-msg-left"});
-
+      this.contatti[this.contattiIndex].chat.push({msg: "ok", date: this.currentTime(), stato: "msg-received", posizione: "box-msg-left"});
     },
+
+    currentTime: function(){
+      var today = new Date();
+      var date = today.getDate()+'/'+(today.getMonth()+1)+'/'+today.getFullYear();
+      var time = today.getHours() + ":" + today.getMinutes();
+      var dateTime = date+' '+time;
+      return dateTime;
+    }
 
   },
 
